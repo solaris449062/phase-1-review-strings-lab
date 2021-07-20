e8e57cdee1d96c6d4a4ddf3db2c91372c6e51df4
Sungoh Park <spark449062@gmail.com>
2021-07-20 01:09:00 -0600

    commit tree 0547ae6ac106e7c0ca8dce6bba58b75ca6cb6c05

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+let currentUser = "user0";
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
